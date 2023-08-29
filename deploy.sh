6fcfaa38d6b5b876b2b2b44032e6759e8ab0de93
chen.yunhao <cyh19991002@gmail.com>
2023-08-29 14:38:15 +0800

    docs: add deploy sh text

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+yarn run docs:build
+
+# 进入生成的文件夹
+cd docs/.vitepress/dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git push -f https://github.com/cloudhao1999/xui master:gh-pages
+
+cd -
